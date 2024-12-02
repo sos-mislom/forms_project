676602addff2446b7b1be7e4efad7ae368c10ffe
Yakovlevash <147636715+Yakovlevash@users.noreply.github.com>
2024-12-02 22:39:35 +0500

    Add Registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Header from './Header/Header';
 import CreateForm from './CreateForm/CreateForm';
 import MainSample1 from './MainSample1/MainSample1';
 import MainSample2 from './MainSample2/MainSample2';
+import Authorization from './Authorization/Authorization';
+import Registration from './Registration/Registration';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/create-form" element={<Main />}/>
         <Route path="/create-form-with-template1" element={<MainSample1 />}/>
         <Route path="/create-form-with-template2" element={<MainSample2 />}/>
+        <Route path="/authorization" element={<Authorization />}/>
+        <Route path="/registration" element={<Registration />}/>
       </Routes>
     </Router>
   );
